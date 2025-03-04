c48f8f82755110092f6ca602a54b7be9b8381ef1
SamserAliKhan <samseralikhan0448@gmail.com>
2025-03-04 05:56:59 +0530

    Removed testENV.ts from tracking

--- backend/testENV.ts
+++
@@ -1,7 +0,0 @@
-import dotenv from "dotenv";
-import path from "path";
-
-// Load .env
-dotenv.config({ path: path.resolve(__dirname, ".env") });
-
-console.log("DB_CONNECTION:", process.env.CENTRALIZED_DB_URI);
